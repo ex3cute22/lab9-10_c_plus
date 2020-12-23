66f522fe7a9cb10c6b7ac922bdecd2d355437ec6
Ilya <viktorov.200@mail.ru>
2020-12-23 14:20:08 +0700

    final2

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -1,20 +1,264 @@
-﻿// lab9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <stdlib.h>
+#include <locale>
+#include <Windows.h>
+#include <conio.h>
+#include <locale.h>
+#define fflush while (getchar() != '\n')
+#define AMOUNTCAR 3 
 
-#include <iostream>
+class car {
+private:
+    std::string marka; //Марка
+    std::string model; //Модель
+    float engine; //Объём двигателя
+    int power; //Кол-во л.с.
+    std::string transmission; //Коробка передач
+    std::string color; //Цвет машины
+    int year; //Год автомобиля
+    int price; //Цена:
+    static int count;
+    std::string* people; //владельцы
+    int countPeople; //кол-во владельцев
+public:
+    car() {
+        marka = " ";
+        model = " ";
+        engine = 0;
+        power = 0;
+        transmission = " ";
+        color = " ";
+        year = 0;
+        price = 0;
+        countPeople = 0;
+        count++;
+    }
+    car(std::string _marka) {
+        marka = " ";
+        marka = _marka;
+        model = " ";
+        engine = 0;
+        power = 0;
+        transmission = " ";
+        color = " ";
+        year = 0;
+        price = 0;
+        countPeople = 0;
+        count++;
+    }
+    car(std::string _marka, std::string _model, float _engine, int _power, std::string _transmission, std::string _color, int _year, int _price, int _countPeople, std::string* _people) {
+        marka = " ";
+        model = " ";
+        marka = _marka;
+        model = _model;
+        engine = _engine;
+        power = _power;
+        transmission = " ";
+        color = " ";
+        transmission = _transmission;
+        color = _color;
+        year = _year;
+        price = _price;
+        countPeople = _countPeople;
+        people = new std::string[countPeople];
+        for (int i = 0; i < countPeople; i++) {
+            people[i] = _people[i];
+        }
+        count++;
+    }
+
+    //Глубокий конструктор копирования
+    car(car& a) {
+        marka = " ";
+        model = " ";
+        marka = a.marka;
+        model = a.model;
+        engine = a.engine;
+        power = a.power;
+        transmission = " ";
+        color = " ";
+        transmission = a.transmission;
+        color = a.color;
+        year = a.year;
+        price = a.price;
+        countPeople = a.countPeople;
+        people = new std::string[countPeople];
+        for (int i = 0; i < countPeople; i++) {
+            people[i] = a.people[i];
+        }
+        count++;
+    }
+    ~car() {
+        car::RemoveItem();
+    }
+    void Read() {
+        printf("Введите марку машины: ");
+        std::cin >> marka;
+        printf("Введите модель машины: ");
+        std::cin >> model;
+        printf("Введите объём двигателя: ");
+        scanf_s("%f", &engine);
+        fflush;
+        printf("Введите кол-во л.с.: ");
+        scanf_s("%d", &power);
+        fflush;
+        printf("Введите тип трансмиссии(коробка передач): ");
+        std::cin >> transmission;
+        printf("Введите цвет автомобиля: ");
+        std::cin >> color;
+        printf("Введите год автомобиля: ");
+        scanf_s("%d", &year);
+        fflush;
+        printf("Введите цену за данный автомобиль: ");
+        scanf_s("%d", &price);
+        fflush;
+        printf("\n");
+    }
+    void Display() {
+        if (marka != " ")std::cout << marka << std::endl;
+        if (model != " ") std::cout << model << std::endl;
+        if (engine != 0) std::cout << engine << std::endl;
+        if (power != 0)std::cout << power << std::endl;
+        if (transmission != " ")std::cout << transmission << std::endl;
+        if (color != " ")std::cout << color << std::endl;
+        if (year != 0)std::cout << year << std::endl;
+        if (price != 0)std::cout << price << std::endl;
+        if (countPeople != 0) {
+            for (int i = 0; i < countPeople; i++)
+                std::cout << people[i] << " ";
+        }
+        std::cout << std::endl << std::endl;
+
+    }
+    void Add(car object) {
+        int sum = price + object.price;
+        if (sum > 350000) {
+            marka = "Ferrari"; model = "F430"; engine = 5.0; power = 440; transmission = "Автомат", color = "Красный", year = 2018; price = 350000;
+        }
+        else if (sum > 250000) {
+            marka = "Nissan"; model = "Skyline"; engine = 3.0; power = 270; transmission = "Автомат", color = "Серебристый", year = 2004; price = 250000;
+        }
+        else if (sum > 150000) {
+            marka = "ВАЗ"; model = "2101"; engine = 1.0; power = 60; transmission = "Механика", color = "Красный", year = 1988; price = 150000;
+        }
+        else {
+            marka = "ЗАЗ"; model = "966"; engine = 0.8; power = 40; transmission = "Механика", color = "Синий", year = 1966; price = 0;
+        }
+    }
+    void Sale(int& _balance) {
+        _balance += price;
+    }
+    void Paint() {
+        std::string newColor;
+        std::cout << "\nСтарый цвет: " << color << "\tНовый цвет: ";
+        std::cin >> newColor;
+        color = newColor;
+    }
+    int* getPrice() {
+        return &price;
+    }
+    int setPrice(int* price) {
+        return *price;
+    }
+    int* getPower() {
+        return &power;
+    }
+    int& setPower(int* power) {
+        return *power;
+    }
+
+    //Дружественная функция
+    friend int getYear(car& CAR);
+    //Перегрузка операторов '+', '++'
+    car& operator++(); // версия префикс
+    car operator++(int); // версия постфикс
+
+    static void RemoveItem() {
+        count--;
+    }
+
+    static int infoCount() {
+        return count;
+    }
+    car& operator= (const car& a)
+    {
+        marka = " ";
+        model = " ";
+        marka = a.marka;
+        model = a.model;
+        engine = a.engine;
+        power = a.power;
+        transmission = " ";
+        color = " ";
+        transmission = a.transmission;
+        color = a.color;
+        year = a.year;
+        price = a.price;
+        countPeople = a.countPeople;
+        people = new std::string[countPeople];
+        for (int i = 0; i < countPeople; i++) {
+            people[i] = a.people[i];
+        }
+        return *this;
+    }
+};
+
+
+
+car& car::operator++() {
+    power++;
+    return *this;
+}
+
+car car::operator++(int) {
+    engine++;
+    return *this;
+}
+
+car operator+(car& a, car& b) {
+    car c = a;
+    c.Add(b);
+    return c;
+}
+
+int getYear(car& CAR) {
+    return CAR.year;
+}
+
+int car::count = 0;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Rus");
+    system("color 70");
+
+    int balance = 0;
+
+    std::string people[2] = { "vova", "petya" };
+    car toyota("toyota", "carib", 1.3, 83, "автомат", "зелёный", 1998, 100000, 2, people);
+    toyota.Display();
+    car bmw("bmw");
+    bmw.Display();
+    car nissan;
+    nissan.Display();
+
+    //car toyota;
+    //toyota.Init("toyota", "carib", 1.3, 83, "автомат", "зелёный", 1998, 100000);
+    //car bmw;
+
+    ////Указатель
+    //int* power = toyota.getPower();
+    ///*(*power)++;*/
+    //toyota.Display();
+    ////Ссылка
+    //toyota.setPower(power) = 300;
+    //toyota++;
+    //++toyota;
+    //++toyota;
+    //toyota.Display();
+
+    //printf("Год: %d", getYear(toyota));
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
